d71fda4ef4c5c13637ff66b87ab813a960c3bc07
Dale Hagglund <dale.hagglund@gmail.com>
2023-09-20 16:27:03 -0600

    teach statement() to use Invoice
    statement() now constructs an invoice from the performance
    data and the play data, and *then* formats that invoice. This
    means that it should now be easy to change `statement()` to
    *return* the invoice and create two new that work on the invoice
    itself.
    
    - introduce new loop to create an invoice from the inputs
    - use the invoice itself to drive the formatting operation,
      as a second phase that will soon be extracted to a new
      function

--- python/statement.py
+++ python/statement.py
@@ -42,29 +42,39 @@ class Invoice:
     def total_credits(self) -> int: return self._total_credits
 
 def statement(invoice, plays):
-    total_amount = 0
-    volume_credits = 0
-    result = f'Statement for {invoice["customer"]}\n'
-    
     plays = {
         id: Play(id, info["name"], info["type"])        
         for id, info in plays.items()
     }
 
+    inv = Invoice(invoice["customer"])
+    for perf in invoice["performances"]:
+        inv.add_performance(plays[perf["playID"]], perf["audience"])
+
     def format_as_dollars(amount):
         return f"${amount:0,.2f}"
+    def statement_title(customer):
+        return f'Statement for {customer}\n'
+    def performance_line(item):
+        name = item.play.name()
+        cost = item.cost
+        audience = item.audience
+        return f' {name}: {format_as_dollars(cost/100)} ({audience} seats)\n'
 
-    for perf in invoice['performances']:
-        play = plays[perf['playID']]
-        
-        this_amount = play.performance_cost(perf['audience'])
-        volume_credits += play.volume_credits(perf['audience'])
-
-        result += f' {play.name()}: {format_as_dollars(this_amount/100)} ({perf["audience"]} seats)\n'
-        total_amount += this_amount
-
-    result += f'Amount owed is {format_as_dollars(total_amount/100)}\n'
-    result += f'You earned {volume_credits} credits\n'
-    return result
-
+    output_lines = []
+    output_lines.append(
+        statement_title(inv.customer())
+    )
+    for item in inv.line_items():
+        output_lines.append(
+            performance_line(item)
+        )
+    output_lines.append(
+        f'Amount owed is {format_as_dollars(inv.total_cost()/100)}\n'
+    )
+    output_lines.append(
+        f'You earned {inv.total_credits()} credits\n'
+    )
+    
+    return "".join(output_lines)
 
